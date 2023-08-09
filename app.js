c150b0e87bc08d7ede3a58b466924084879245d0
Allan Fernandes <Fernzallan14@gmail.com>
2023-08-09 17:18:52 +0100

    Episode-2

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
-  { class: "parent" },
+  { id: "parent" },
   React.createElement("div", {}, [
     React.createElement("h1", {}, "this is child div"),
-    React.createElement("h2", {}, "this is sibling of the child div"),
+    React.createElement("h2", {}, "this is sibling of the lavdeeeee div"),
   ])
 );
 
