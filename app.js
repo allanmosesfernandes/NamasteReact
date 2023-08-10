8f28f146f27a959502fa589e77dd6ea5d53c4f42
Allan Fernandes <Fernzallan14@gmail.com>
2023-08-10 01:38:12 +0100

    episode-3

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", {}, [
-    React.createElement("h1", {}, "this is child div"),
-    React.createElement("h2", {}, "this is sibling of the lavdeeeee div"),
-  ])
+// this is JSX code
+const heading = (
+  <h1>
+    Hello World
+    <span>from React</span>
+  </h1>
 );
 
+/* Functional Component */
+const Title = () => <h3>this is from the title component</h3>;
+
+const Heading = () => {
+  return (
+    <div className="container">
+      <Title />
+      <h1>Hello World from functional comp</h1>
+    </div>
+  );
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<Heading />);
